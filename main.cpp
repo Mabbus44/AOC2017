283b8ac4ba4e305a86f33b39aa55097c52e3294a
Mabbus44 <rasun44@hotmail.com>
2017-12-11 10:25:07 +0100

    Dag 11

--- main.cpp
+++ main.cpp
@@ -716,6 +716,59 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
                       out << "Day " << selectedDay << "\nPart 1: " << ans1 << "\nPart 2: " << strAns2;
                       break;
                     }
+//*************************************** DAY 11 ***************************************
+                    case 11:
+                    {
+                      std::string line;
+                      std::getline(file, line);
+                      std::replace(line.begin(), line.end(), ',', ' ');
+                      std::stringstream ss(line);
+                      std::string dir;
+                      int x = 0;
+                      int y = 0;
+                      while (ss >> dir)
+                      {
+                        if(dir.compare("s") == 0)
+                          y-=2;
+                        if(dir.compare("se") == 0)
+                        {
+                          y--;
+                          x++;
+                        }
+                        if(dir.compare("ne") == 0)
+                        {
+                          y++;
+                          x++;
+                        }
+                        if(dir.compare("n") == 0)
+                          y+=2;
+                        if(dir.compare("nw") == 0)
+                        {
+                          y++;
+                          x--;
+                        }
+                        if(dir.compare("sw") == 0)
+                        {
+                          y--;
+                          x--;
+                        }
+                        int distX = std::abs(x);
+                        int distY = std::abs(y);
+                        int dist = distX;
+                        distY -= distX;
+                        if(distY>0)
+                          dist += distY/2;
+                        if(dist > ans2)
+                          ans2 = dist;
+                      }
+                      ans1 = std::abs(x);
+                      y = std::abs(y);
+                      y -= ans1;
+                      if(y>0)
+                        ans1 += y/2;
+                      out << "Day " << selectedDay << "\nPart 1: " << ans1 << "\nPart 2: " << ans2;
+                      break;
+                    }
                     default:
                       break;
                   }
