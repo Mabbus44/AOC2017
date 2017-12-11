c2d5c486bd8f5d9d91b8c1e1dd0c4a0e53b687d2
Mabbus44 <rasun44@hotmail.com>
2017-12-11 09:54:04 +0100

    Dag 10

--- windowObjects.cpp
+++ windowObjects.cpp
@@ -25,7 +25,7 @@ LRESULT CALLBACK subEditProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 void defineWindowObjects(HWND hwnd)
 {
   CreateWindow("BUTTON", "Load file", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 120, 10, 100, 20, hwnd, (HMENU)T000_OPEN_FILE, (HINSTANCE)GetWindowLong(hwnd, GWL_HINSTANCE), NULL);
-  CreateWindow("STATIC", "Calculation not started", WS_TABSTOP | WS_VISIBLE | WS_CHILD, 230, 10, 150, 60, hwnd, (HMENU)T000_ANS, (HINSTANCE)GetWindowLong(hwnd, GWL_HINSTANCE), NULL);
+  CreateWindow("STATIC", "Calculation not started", WS_TABSTOP | WS_VISIBLE | WS_CHILD, 230, 10, 250, 160, hwnd, (HMENU)T000_ANS, (HINSTANCE)GetWindowLong(hwnd, GWL_HINSTANCE), NULL);
   HWND hwndCB = CreateWindow("COMBOBOX", "", CBS_HASSTRINGS | WS_OVERLAPPED | CBS_DROPDOWNLIST | WS_CHILD | WS_VISIBLE, 10, 10, 100, 600, hwnd, (HMENU)T000_DAY_SELECTION, (HINSTANCE)GetWindowLong(hwnd, GWL_HINSTANCE), NULL);
   SendMessage(hwndCB, CB_ADDSTRING, 0, (LPARAM)"Day 1");
   SendMessage(hwndCB, CB_ADDSTRING, 0, (LPARAM)"Day 2");
@@ -36,6 +36,7 @@ void defineWindowObjects(HWND hwnd)
   SendMessage(hwndCB, CB_ADDSTRING, 0, (LPARAM)"Day 7");
   SendMessage(hwndCB, CB_ADDSTRING, 0, (LPARAM)"Day 8");
   SendMessage(hwndCB, CB_ADDSTRING, 0, (LPARAM)"Day 9");
+  SendMessage(hwndCB, CB_ADDSTRING, 0, (LPARAM)"Day 10");
   SendMessage(hwndCB, CB_SETCURSEL, 0, 0);
   HWND hwndEdit = CreateWindowEx(WS_EX_CLIENTEDGE, "EDIT", "", WS_CHILD | WS_BORDER | ES_LEFT, 120, 10, 100, 20, hwnd, (HMENU)T030_INPUT, (HINSTANCE)GetWindowLong(hwnd, GWL_HINSTANCE), NULL);
   oldEditProc = (WNDPROC)SetWindowLongPtr(hwndEdit, GWLP_WNDPROC, (LONG_PTR)subEditProc);
