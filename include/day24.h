03ddcab3e82d32abaf62552e322ea28148a5c12e
Mabbus44 <rasun44@hotmail.com>
2018-02-19 19:03:24 +0100

    Dag 24

--- include/day24.h
+++ include/day24.h
@@ -6,7 +6,23 @@
 #include <sstream>
 #include <stdlib.h>
 #include <iostream>
+#include <vector>
 
 std::string day24(std::ifstream& file);
+
+class Day24Part
+{
+public:
+  int sides[2];
+};
+
+class Day24Bridge
+{
+public:
+  std::vector<Day24Part> parts;
+  void loadPart(std::string line);
+  int strongest(std::vector<int> usedParts, int conStr);
+  void longest(std::vector<int> usedParts, int conStr, int& length, int& strength);
+};
 
 #endif // DAY24_H_INCLUDED
--- src/day24.cpp
+++ src/day24.cpp
@@ -2,9 +2,105 @@
 
 std::string day24(std::ifstream& file)
 {
-  std::string ans1, ans2;
-
+  int ans1, ans2;
+  std::string line;
+  Day24Bridge bridge;
+  while(std::getline(file, line))
+  {
+    if(line.length() > 0)
+    {
+      bridge.loadPart(line);
+    }
+  }
+  std::vector<int> emptyVector, emptyVector2;
+  ans1 = bridge.strongest(emptyVector, 0);
+  int strength = 0;
+  int length = 0;
+  bridge.longest(emptyVector2, 0, length, strength);
+  ans2 = strength;
   std::stringstream out;
   out << "Day 24\nPart 1: " << ans1 << "\nPart 2: " << ans2;
   return out.str();
+}
+
+void Day24Bridge::loadPart(std::string line)
+{
+  Day24Part part;
+  int dividePos = line.find('/');
+  part.sides[0] = atoi(line.substr(0,dividePos).c_str());
+  part.sides[1] = atoi(line.substr(dividePos+1, line.length()-dividePos-1).c_str());
+  parts.push_back(part);
+}
+
+int Day24Bridge::strongest(std::vector<int> usedParts, int conStr)
+{
+  bool fit;
+  int maxStr = 0;
+  for(int i=0; i<(int)(parts.size()); i++)
+  {
+    if(parts[i].sides[0] == conStr || parts[i].sides[1] == conStr)
+      fit = true;
+    else
+      fit = false;
+    for(int i2=0; i2<(int)(usedParts.size()) && fit; i2++)
+    {
+      if(i==usedParts[i2])
+        fit = false;
+    }
+    if(fit)
+    {
+      std::vector<int> newUsedParts = usedParts;
+      int newConStr;
+      newUsedParts.push_back(i);
+      if(parts[i].sides[0] == conStr)
+        newConStr = parts[i].sides[1];
+      else
+        newConStr = parts[i].sides[0];
+      int str = strongest(newUsedParts, newConStr);
+      if(str>maxStr)
+        maxStr = str;
+    }
+  }
+  if(usedParts.size()>0)
+    maxStr = maxStr + parts[usedParts[usedParts.size()-1]].sides[0] + parts[usedParts[usedParts.size()-1]].sides[1];
+  return maxStr;
+}
+
+void Day24Bridge::longest(std::vector<int> usedParts, int conStr, int& length, int& strength)
+{
+  bool fit;
+  for(int i=0; i<(int)(parts.size()); i++)
+  {
+    if(parts[i].sides[0] == conStr || parts[i].sides[1] == conStr)
+      fit = true;
+    else
+      fit = false;
+    for(int i2=0; i2<(int)(usedParts.size()) && fit; i2++)
+    {
+      if(i==usedParts[i2])
+        fit = false;
+    }
+    if(fit)
+    {
+      std::vector<int> newUsedParts = usedParts;
+      int newConStr;
+      newUsedParts.push_back(i);
+      if(parts[i].sides[0] == conStr)
+        newConStr = parts[i].sides[1];
+      else
+        newConStr = parts[i].sides[0];
+      longest(newUsedParts, newConStr, length, strength);
+    }
+  }
+  if((int)(usedParts.size())>=length)
+  {
+    int thisStrength = 0;
+    for(int i=0; i<(int)(usedParts.size()); i++)
+      thisStrength += (parts[usedParts[i]].sides[0] + parts[usedParts[i]].sides[1]);
+    if((int)(usedParts.size())>length || ((int)(usedParts.size()) == length && thisStrength>strength))
+    {
+      length = (int)(usedParts.size());
+      strength = thisStrength;
+    }
+  }
 }
